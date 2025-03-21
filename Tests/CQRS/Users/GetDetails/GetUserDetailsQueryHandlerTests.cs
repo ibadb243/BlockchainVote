8e15ef6086e666653fee5cdd557e0f2c36c455af
IbadDesktop <kronosxplay243@gmail.com>
2025-03-22 03:10:01 +0400

    Add Tests for GetUserDetailsQueryHandler, GetUserDetailsQueryValidator

---
+++ Tests/CQRS/Users/GetDetails/GetUserDetailsQueryHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using Application.Common.Exceptions;
+using Application.CQRS.Users.Queries.GetDetailsQuery;
+using AutoMapper;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Persistence;
+
+namespace Tests.CQRS.Users.GetDetails;
+
+public class GetUserDetailsQueryHandlerTests
+{
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly GetUserDetailsQueryHandler _handler;
+    private readonly ApplicationDbContext _context;
+
+    public GetUserDetailsQueryHandlerTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "userget")
+            .Options;
+
+        _context = new ApplicationDbContext(options);
+
+        _mapperMock = new Mock<IMapper>();
+        _mapperMock.Setup(x => x.Map<UserVm>(It.IsAny<User>())).Returns(new UserVm() { CreatedAt = DateTimeOffset.MaxValue });
+
+        _handler = new GetUserDetailsQueryHandler(_context, _mapperMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnUserVm_WhenValidRequest()
+    {
+        // Arrange
+        var user = new User("hash", "hash");
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+        var query = new GetUserDetailsQuery { UserId = user.Id };
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(result.CreatedAt, DateTimeOffset.MaxValue);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowNotFoundException_WhenUserNotFound()
+    {
+        // Arrange
+        var query = new GetUserDetailsQuery { UserId = Guid.Empty };
+
+        // Act
+        var exception = await Assert.ThrowsAsync<NotFoundException<User>>(() => _handler.Handle(query, CancellationToken.None));
+
+        // Assert
+        Assert.Equal("User not found!", exception.Message);
+    }
+}
---
+++ Tests/CQRS/Users/GetDetails/GetUserDetailsQueryValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using Application.Common.Behaviors;
+using Application.CQRS.Users.Queries.GetDetailsQuery;
+using FluentValidation;
+using MediatR;
+using Moq;
+
+namespace Tests.CQRS.Users.GetDetails;
+
+public class GetUserDetailsQueryValidatorTests
+{
+    private readonly ValidationBehavior<GetUserDetailsQuery, UserVm> _behavior;
+    private readonly Mock<RequestHandlerDelegate<UserVm>> _nextMock;
+    private readonly List<IValidator<GetUserDetailsQuery>> _validators;
+
+    public GetUserDetailsQueryValidatorTests()
+    {
+        _validators = new List<IValidator<GetUserDetailsQuery>>() { new GetUserDetailsQueryValidator() };
+        _nextMock = new Mock<RequestHandlerDelegate<UserVm>>();
+        _behavior = new ValidationBehavior<GetUserDetailsQuery, UserVm>(_validators);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowValidationException_WhenInvalidRequest()
+    {
+        // Arrange
+        var query = new GetUserDetailsQuery { UserId = Guid.Empty };
+
+        // Act
+        var exception = await Assert.ThrowsAsync<ValidationException>(() => _behavior.Handle(query, _nextMock.Object, CancellationToken.None));
+
+        // Assert
+        Assert.Single(exception.Errors);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCallNext_WhenValidRequest()
+    {
+        // Arrange
+        var query = new GetUserDetailsQuery { UserId = Guid.NewGuid() };
+        _nextMock.Setup(x => x()).ReturnsAsync(new UserVm() { CreatedAt = DateTimeOffset.MaxValue });
+
+        // Act
+        var result = await _behavior.Handle(query, _nextMock.Object, CancellationToken.None);
+
+        // Assert
+        _nextMock.Verify(x => x(), Times.Once);
+        Assert.NotNull(result);
+        Assert.Equal(DateTimeOffset.MaxValue, result.CreatedAt);
+    }
+}
