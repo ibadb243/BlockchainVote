3a82cf701be6bbc799def5a407e77e1b1a72926f
IbadDesktop <kronosxplay243@gmail.com>
2025-03-22 01:42:22 +0400

    DependencyInjection.cs in Application Layer

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Behaviors;
+using Application.Common.Services;
+using Application.Interfaces;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+        });
+
+        services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        services.AddTransient<IBlockBuilder, BlockBuilder>();
+        services.AddTransient<IHashHelper, HashHelper>();
+
+        return services;
+    }
+}
