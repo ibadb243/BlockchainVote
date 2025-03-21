102ef0b022f63c9059cbfb5d12c472e337820c6b
IbadDesktop <kronosxplay243@gmail.com>
2025-03-22 02:04:41 +0400

    PollOptionConfiguration

---
+++ Persistence/EntityTypeCoonfigurations/PollOptionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityTypeCoonfigurations;
+
+public class PollOptionConfiguration : IEntityTypeConfiguration<PollOption>
+{
+    public void Configure(EntityTypeBuilder<PollOption> builder)
+    {
+        builder.HasIndex(x => x.PollId);
+
+        builder.Property(x => x.Fullname).HasMaxLength(256).IsRequired();
+
+        builder.Property(x => x.Description).HasMaxLength(4096);
+
+        builder.Property(x => x.ImagePath).HasMaxLength(256);
+    }
+}
