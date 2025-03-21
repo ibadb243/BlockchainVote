31ed98a1f6f12ddc2a3f6ad95d2bcb6d62514bc0
IbadDesktop <kronosxplay243@gmail.com>
2025-03-22 03:08:55 +0400

    Add Tests for CreateUserCommandHandler, CreateUserCommandValidator

---
+++ Tests/CQRS/Users/Create/CreateUserCommandHandlerTests.cs
@@ -0,0 +1,64 @@
+﻿using Application.CQRS.Users.Commands.CreateCommand;
+using Application.Interfaces;
+using Persistence;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using Application.Common.Exceptions;
+using Domain.Entities;
+
+namespace Tests.CQRS.Users.Create;
+
+public class CreateUserCommandHandlerTests
+{
+    private readonly Mock<IHashHelper> _hashHelperMock;
+    private readonly CreateUserCommandHandler _handler;
+    private readonly ApplicationDbContext _context;
+
+    public CreateUserCommandHandlerTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "usercreate")
+            .Options;
+
+        _context = new ApplicationDbContext(options);
+
+        _hashHelperMock = new Mock<IHashHelper>();
+        _hashHelperMock.Setup(x => x.CalculateHash(It.IsAny<string>())).Returns("hash");
+
+        _handler = new CreateUserCommandHandler(_context, _hashHelperMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCreateUserCommand_WhenValidRequest()
+    {
+        // Arrange
+        var command = new CreateUserCommand { FIN = "ABCDEFG", Password = "PASSWORD" };
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.NotEqual(Guid.Empty, result);
+
+        var user = await _context.Users.FirstOrDefaultAsync();
+
+        Assert.Equal(result, user.Id);
+        Assert.Equal("hash", user.FINHash);
+        Assert.Equal("hash", user.FINHash);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCreateUserCommand_WhenFinIsAlreadyUsed()
+    {
+        // Arrange
+        var command = new CreateUserCommand { FIN = "ABCDEFG", Password = "PASSWORD" };
+        await _context.Users.AddAsync(new User("hash", "hash"));
+        await _context.SaveChangesAsync();
+
+        // Act
+        var exception = await Assert.ThrowsAsync<FinIsAlreadyUsedException>(() => _handler.Handle(command, CancellationToken.None));
+
+        // Assert
+        Assert.Equal("FIN ABCDEFG is already used!", exception.Message);
+    }
+}
---
+++ Tests/CQRS/Users/Create/CreateUserCommandValidatorTests.cs
@@ -0,0 +1,49 @@
+﻿using Application.Common.Behaviors;
+using Application.CQRS.Users.Commands.CreateCommand;
+using FluentValidation;
+using MediatR;
+using Moq;
+
+namespace Tests.CQRS.Users.Create;
+
+public class CreateUserCommandValidatorTests
+{
+    private readonly ValidationBehavior<CreateUserCommand, Guid> _behavior;
+    private readonly Mock<RequestHandlerDelegate<Guid>> _nextMock;
+    private readonly List<IValidator<CreateUserCommand>> _validators;
+
+    public CreateUserCommandValidatorTests()
+    {
+        _validators = new List<IValidator<CreateUserCommand>>() { new CreateUserCommandValidator() };
+        _nextMock = new Mock<RequestHandlerDelegate<Guid>>();
+        _behavior = new ValidationBehavior<CreateUserCommand, Guid>(_validators);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowValidationException_WhenInvalidRequest()
+    {
+        // Arrange
+        var command = new CreateUserCommand { FIN = "", Password = "" };
+
+        // Act
+        var exception = await Assert.ThrowsAsync<ValidationException>(() => _behavior.Handle(command, _nextMock.Object, CancellationToken.None));
+
+        // Assert
+        Assert.Equal(2, exception.Errors.Count());
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCallNext_WhenValidRequest()
+    {
+        // Arrange
+        var command = new CreateUserCommand { FIN = "ABCDEFG", Password = "PASSWORD" };
+        _nextMock.Setup(x => x()).ReturnsAsync(Guid.Parse("aba44968-f62e-433c-9701-ee99fe37de80"));
+
+        // Act
+        var result = await _behavior.Handle(command, _nextMock.Object, CancellationToken.None);
+
+        // Assert
+        _nextMock.Verify(x => x(), Times.Once);
+        Assert.Equal(Guid.Parse("aba44968-f62e-433c-9701-ee99fe37de80"), result);
+    }
+}
