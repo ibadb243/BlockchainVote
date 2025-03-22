8b81cdf6e3845b6ce74e13ecb67a667276710a7b
IbadDesktop <kronosxplay243@gmail.com>
2025-03-23 03:54:38 +0400

    Add Creating SingleChoicePoll CommandHandler

---
+++ Application/CQRS/Polls/Commands/CreateCommand/Single/CreateSingleChoicePollCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Application.CQRS.Polls.Commands.CreateCommand.Single;
+
+public class CreateSingleChoicePollCommand : IRequest<Guid>
+{
+    public Guid UserId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public bool IsAnonymous { get; set; } = false;
+    public DateTimeOffset StartDate { get; set; }
+    public DateTimeOffset? EndDate { get; set; }
+    public List<OptionDto>  Options { get; set; } = new List<OptionDto>();
+}
---
+++ Application/CQRS/Polls/Commands/CreateCommand/Single/CreateSingleChoicePollCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.CQRS.Polls.Commands.CreateCommand.Single;
+
+public class CreateSingleChoicePollCommandHandler : IRequestHandler<CreateSingleChoicePollCommand, Guid>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateSingleChoicePollCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Guid> Handle(CreateSingleChoicePollCommand request, CancellationToken cancellationToken)
+    {
+        var poll = new SingleChoicePoll(
+            request.UserId,
+            request.Title,
+            request.Description,
+            request.StartDate,
+            request.EndDate,
+            request.IsAnonymous);
+
+        await _context.SingleChoicePolls.AddAsync(poll, cancellationToken);
+
+        for (int i = 0; i < request.Options.Count; i++)
+        {
+            var option = new PollOption(
+                poll.Id,
+                1 << i,
+                request.Options[i].Fullname,
+                request.Options[i].Description,
+                request.Options[i].ImagePath);
+
+            await _context.PollOption.AddAsync(option,cancellationToken);
+        }
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return poll.Id;
+    }
+}
