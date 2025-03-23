f65e56200bd1c56e1a465c5035e532ddc3acbb6c
IbadDesktop <kronosxplay243@gmail.com>
2025-03-23 04:10:56 +0400

    Tests: CreateSingleChoicePollCommandHandler, CreateSingleChoicePollCommandValidator

---
+++ Tests/CQRS/Polls/Create/CreateSingleChoicePollCommandHandlerTests.cs
@@ -0,0 +1,106 @@
+﻿using Application.CQRS.Polls.Commands.CreateCommand.Single;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Persistence;
+
+namespace Tests.CQRS.Polls.Create;
+
+public class CreateSingleChoicePollCommandHandlerTests
+{
+    private readonly CreateSingleChoicePollCommandHandler _handler;
+    private readonly ApplicationDbContext _context;
+
+    public CreateSingleChoicePollCommandHandlerTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "usercreate")
+            .Options;
+
+        _context = new ApplicationDbContext(options);
+
+        _handler = new CreateSingleChoicePollCommandHandler(_context);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCreateSingleChoicePoll_WhenValidRequest()
+    {
+        // Arrange
+        await reset_database();
+        var command = new CreateSingleChoicePollCommand()
+        {
+            UserId = Guid.Parse("fe560d8e-dbe4-4e5e-b1b2-da08b8145494"),
+            Title = "Title123",
+            Description = "Description",
+            StartDate = DateTimeOffset.UtcNow,
+            EndDate = DateTimeOffset.UtcNow.AddHours(1),
+            IsAnonymous = false,
+            Options = new List<OptionDto>()
+            {
+                new OptionDto() { Fullname="Fullname", Description="Description" },
+                new OptionDto() { Fullname="Fullname", Description="Description" },
+                new OptionDto() { Fullname="Fullname", Description="Description" },
+            }
+        };
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.NotEqual(Guid.Empty, result);
+        var poll = await _context.SingleChoicePolls.FirstOrDefaultAsync(CancellationToken.None);
+        Assert.NotNull(poll);
+        Assert.Equal(Guid.Parse("fe560d8e-dbe4-4e5e-b1b2-da08b8145494"), poll.UserId);
+
+        var options = await _context.PollOption.Where(x => x.PollId == poll.Id).ToListAsync();
+        Assert.Equal(3, options.Count);
+    }
+
+    /// <summary>
+    /// The request must be pass by validator before getted by handle
+    /// </summary>
+    [Fact]
+    public async Task Handle_ShouldCreateSingleChoicePoll_WhenInvalidRequest()
+    {
+        // Arrange
+        await reset_database();
+        var command = new CreateSingleChoicePollCommand()
+        {
+            Title = string.Empty,
+            Options = new List<OptionDto>()
+        };
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.NotEqual(Guid.Empty, result);
+        var poll = await _context.SingleChoicePolls.FirstOrDefaultAsync(CancellationToken.None);
+        Assert.NotNull(poll);
+        var options = await _context.PollOption.Where(x => x.PollId == poll.Id).ToListAsync();
+        Assert.Empty(options);
+    }
+
+    /// <summary>
+    /// The request must be pass by validator before getted by handle
+    /// </summary>
+    [Fact]
+    public async Task Handle_ThrowDbContextExceptions_WhenEmptyRequest()
+    {
+        // Arrange
+        await reset_database();
+        var command = new CreateSingleChoicePollCommand();
+
+        // Act
+        var exception = await Assert.ThrowsAsync<DbUpdateException>(async () => await _handler.Handle(command, CancellationToken.None));
+
+        // Assert
+        Assert.NotNull(exception);
+        Assert.Contains("{'Title'}", exception.Message);
+    }
+
+    private async Task reset_database()
+    {
+        await _context.Database.EnsureDeletedAsync();
+        await _context.Database.EnsureCreatedAsync();
+    }
+}
---
+++ Tests/CQRS/Polls/Create/CreateSingleChoicePollCommandValidatorTests.cs
@@ -0,0 +1,101 @@
+﻿using Application.Common.Behaviors;
+using Application.CQRS.Polls.Commands.CreateCommand.Single;
+using FluentValidation;
+using MediatR;
+using Moq;
+
+namespace Tests.CQRS.Polls.Create;
+
+public class CreateSingleChoicePollCommandValidatorTests
+{
+    private readonly ValidationBehavior<CreateSingleChoicePollCommand, Guid> _behavior;
+    private readonly Mock<RequestHandlerDelegate<Guid>> _nextMock;
+    private readonly List<IValidator<CreateSingleChoicePollCommand>> _validators;
+
+    public CreateSingleChoicePollCommandValidatorTests()
+    {
+        _validators = new List<IValidator<CreateSingleChoicePollCommand>>() { new CreateSingleChoicePollCommandValidator() };
+        _nextMock = new Mock<RequestHandlerDelegate<Guid>>();
+        _behavior = new ValidationBehavior<CreateSingleChoicePollCommand, Guid>(_validators);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowValidationException_WhenEmptyRequest()
+    {
+        // Arrange
+        var command = new CreateSingleChoicePollCommand();
+
+        // Act
+        var exception = await Assert.ThrowsAsync<ValidationException>(() => _behavior.Handle(command, _nextMock.Object, CancellationToken.None));
+
+        // Assert
+        Assert.Equal(4, exception.Errors.Count());
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowValidationException_WhenInvaliddRequest()
+    {
+        // Arrange
+        var command = new CreateSingleChoicePollCommand()
+        {
+            UserId = Guid.NewGuid(),
+            Title = "THISTITLE",
+            StartDate = DateTime.UtcNow,
+            Options = new List<OptionDto>() { } // <-- Invalid
+        };
+
+        // Act
+        var exception = await Assert.ThrowsAsync<ValidationException>(() => _behavior.Handle(command, _nextMock.Object, CancellationToken.None));
+
+        // Assert
+        Assert.Single(exception.Errors);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowValidationException_WhenInvaliddRequest2()
+    {
+        // Arrange
+        var command = new CreateSingleChoicePollCommand()
+        {
+            UserId = Guid.NewGuid(),
+            Title = "THISTITLE",
+            StartDate = DateTime.UtcNow,
+            Options = new List<OptionDto>()
+            {
+                new OptionDto() { Fullname="123" } // <-- Invalid
+            }
+        };
+
+        // Act
+        var exception = await Assert.ThrowsAsync<ValidationException>(() => _behavior.Handle(command, _nextMock.Object, CancellationToken.None));
+
+        // Assert
+        Assert.Single(exception.Errors);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCallNext_WhenValidRequest()
+    {
+        // Arrange
+        var command = new CreateSingleChoicePollCommand()
+        {
+            UserId = Guid.NewGuid(),
+            Title = "THISTITLE",
+            StartDate = DateTime.UtcNow,
+            Options = new List<OptionDto>()
+            {
+                new OptionDto() { Fullname="Option" },
+                new OptionDto() { Fullname="Option" },
+                new OptionDto() { Fullname="Option" },
+            }
+        };
+        _nextMock.Setup(x => x()).ReturnsAsync(Guid.Parse("aba44968-f62e-433c-9701-ee99fe37de80"));
+
+        // Act
+        var result = await _behavior.Handle(command, _nextMock.Object, CancellationToken.None);
+
+        // Assert
+        _nextMock.Verify(x => x(), Times.Once);
+        Assert.Equal(Guid.Parse("aba44968-f62e-433c-9701-ee99fe37de80"), result);
+    }
+}
