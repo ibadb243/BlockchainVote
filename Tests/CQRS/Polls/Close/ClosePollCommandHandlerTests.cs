156682a249ea11c3637966bcf5e9ba227e1f1150
IbadDesktop <kronosxplay243@gmail.com>
2025-03-24 02:55:41 +0400

    Add Tests  for ClosePollCommandHandler, ClosePollCommandValidator

---
+++ Tests/CQRS/Polls/Close/ClosePollCommandHandlerTests.cs
@@ -0,0 +1,170 @@
+﻿using Application.Common.Exceptions;
+using Application.CQRS.Polls.Commands.CloseCommand;
+using Domain.Entities;
+using Domain.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Tests.CQRS.Polls.Close;
+
+public class ClosePollCommandHandlerTests
+{
+    private readonly ClosePollCommandHandler _handler;
+    private readonly ApplicationDbContext _context;
+
+    public ClosePollCommandHandlerTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "pollclose")
+            .Options;
+
+        _context = new ApplicationDbContext(options);
+
+        _handler = new ClosePollCommandHandler(_context);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldClosePoll_WhenValidRequest()
+    {
+        // Arrange
+        await reset_database();
+        var poll = new SingleChoicePoll
+        {
+            Id = Guid.Parse("35e9803c-2af0-4a08-bafd-af3827b4e0d7"),
+            UserId = Guid.Parse("fee6c730-0e98-4dc8-8f70-ca009beea506"),
+            Title = "Title"
+        };
+        await _context.SingleChoicePolls.AddAsync(poll);
+        await _context.SaveChangesAsync();
+        var command = new ClosePollCommand
+        {
+            UserId = poll.UserId,
+            PollId = poll.Id,
+        };
+
+        // Act
+        await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        var singleP = await _context.SingleChoicePolls.FirstOrDefaultAsync(p => p.Id == poll.Id);
+        Assert.NotNull(singleP);
+        Assert.NotNull(singleP.EndDate);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCloseUnclosedPoll_WhenValidRequest()
+    {
+        // Arrange
+        await reset_database();
+        var poll = new SingleChoicePoll
+        {
+            Id = Guid.Parse("35e9803c-2af0-4a08-bafd-af3827b4e0d7"),
+            UserId = Guid.Parse("fee6c730-0e98-4dc8-8f70-ca009beea506"),
+            Title = "Title",
+            EndDate = DateTime.Now.AddDays(5),
+        };
+        await _context.SingleChoicePolls.AddAsync(poll);
+        await _context.SaveChangesAsync();
+        var command = new ClosePollCommand
+        {
+            UserId = poll.UserId,
+            PollId = poll.Id,
+        };
+
+        // Act
+        await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        var singleP = await _context.SingleChoicePolls.FirstOrDefaultAsync(p => p.Id == poll.Id);
+        Assert.NotNull(singleP);
+        Assert.NotNull(singleP.EndDate);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowPollClosedException_WhenValidRequest()
+    {
+        // Arrange
+        await reset_database();
+        var poll = new SingleChoicePoll
+        {
+            Id = Guid.Parse("35e9803c-2af0-4a08-bafd-af3827b4e0d7"),
+            UserId = Guid.Parse("fee6c730-0e98-4dc8-8f70-ca009beea506"),
+            Title = "Title",
+            EndDate = DateTime.Now.AddMinutes(-5),
+        };
+        await _context.SingleChoicePolls.AddAsync(poll);
+        await _context.SaveChangesAsync();
+        var command = new ClosePollCommand
+        {
+            UserId = poll.UserId,
+            PollId = poll.Id,
+        };
+
+        // Act
+        var exception = await Assert.ThrowsAsync<PollClosedException>(async () => await _handler.Handle(command, CancellationToken.None));
+
+        // Assert
+        Assert.NotNull(exception);
+        Assert.Equal("The Poll is already closed!", exception.Message);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowNotFoundException_WhenUsersIdNotMatch()
+    {
+        // Arrange
+        await reset_database();
+        var poll = new SingleChoicePoll
+        {
+            Id = Guid.Parse("35e9803c-2af0-4a08-bafd-af3827b4e0d7"),
+            UserId = Guid.Parse("fee6c730-0e98-4dc8-8f70-ca009beea506"),
+            Title = "Title",
+            EndDate = DateTime.Now.AddMinutes(-5),
+        };
+        await _context.SingleChoicePolls.AddAsync(poll);
+        await _context.SaveChangesAsync();
+        var command = new ClosePollCommand
+        {
+            UserId = Guid.NewGuid(),
+            PollId = poll.Id,
+        };
+
+        // Act
+        var exception = await Assert.ThrowsAsync<NotFoundException<PollBase>>(async () => await _handler.Handle(command, CancellationToken.None));
+
+        // Assert
+        Assert.NotNull(exception);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowNotFoundException_WhenPollsIdNotMatch()
+    {
+        // Arrange
+        await reset_database();
+        var poll = new SingleChoicePoll
+        {
+            Id = Guid.Parse("35e9803c-2af0-4a08-bafd-af3827b4e0d7"),
+            UserId = Guid.Parse("fee6c730-0e98-4dc8-8f70-ca009beea506"),
+            Title = "Title",
+            EndDate = DateTime.Now.AddMinutes(-5),
+        };
+        await _context.SingleChoicePolls.AddAsync(poll);
+        await _context.SaveChangesAsync();
+        var command = new ClosePollCommand
+        {
+            UserId = poll.UserId,
+            PollId = Guid.NewGuid(),
+        };
+
+        // Act
+        var exception = await Assert.ThrowsAsync<NotFoundException<PollBase>>(async () => await _handler.Handle(command, CancellationToken.None));
+
+        // Assert
+        Assert.NotNull(exception);
+    }
+
+    private async Task reset_database()
+    {
+        await _context.Database.EnsureDeletedAsync();
+        await _context.Database.EnsureCreatedAsync();
+    }
+}
---
+++ Tests/CQRS/Polls/Close/ClosePollCommandValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using Application.Common.Behaviors;
+using Application.CQRS.Polls.Commands.CloseCommand;
+using FluentValidation;
+using MediatR;
+using Moq;
+
+namespace Tests.CQRS.Polls.Close;
+
+public class ClosePollCommandValidatorTests
+{
+    private readonly ValidationBehavior<ClosePollCommand, Unit> _behavior;
+    private readonly Mock<RequestHandlerDelegate<Unit>> _nextMock;
+    private readonly List<IValidator<ClosePollCommand>> _validators;
+
+    public ClosePollCommandValidatorTests()
+    {
+        _validators = new List<IValidator<ClosePollCommand>>() { new ClosePollCommandValidator() };
+        _nextMock = new Mock<RequestHandlerDelegate<Unit>>();
+        _behavior = new ValidationBehavior<ClosePollCommand, Unit>(_validators);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowValidationException_WhenInvalidRequest2()
+    {
+        // Arrange
+        var command = new ClosePollCommand();
+
+        // Act
+        var exception = await Assert.ThrowsAsync<ValidationException>(() => _behavior.Handle(command, _nextMock.Object, CancellationToken.None));
+
+        // Assert
+        Assert.Equal(2, exception.Errors.Count());
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCallNext_WhenValidRequest()
+    {
+        // Arrange
+        var command = new ClosePollCommand()
+        {
+            UserId = Guid.NewGuid(),
+            PollId = Guid.NewGuid(),
+            
+        };
+        _nextMock.Setup(x => x()).ReturnsAsync(Unit.Value);
+
+        // Act
+        var result = await _behavior.Handle(command, _nextMock.Object, CancellationToken.None);
+
+        // Assert
+        _nextMock.Verify(x => x(), Times.Once);
+        Assert.Equal(Unit.Value, result);
+    }
+}
