432acd0a229c459e22e918f9bea5a414d970d21b
IbadDesktop <kronosxplay243@gmail.com>
2025-03-24 23:48:01 +0400

    Add Vote, SingleVote and MultipleVote EntityTypeConfigurations

---
+++ Persistence/EntityTypeCoonfigurations/SingleVoteConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityTypeCoonfigurations;
+
+public class SingleVoteConfiguration : IEntityTypeConfiguration<SingleVote>
+{
+    public void Configure(EntityTypeBuilder<SingleVote> builder)
+    {
+        
+    }
+}
---
+++ Persistence/EntityTypeCoonfigurations/VoteConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Domain.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityTypeCoonfigurations;
+
+public class VoteConfiguration : IEntityTypeConfiguration<VoteBase>
+{
+    public void Configure(EntityTypeBuilder<VoteBase> builder)
+    {
+        builder.HasDiscriminator<string>("VoteType")
+            .HasValue<SingleVote>("Single")
+            .HasValue<MultipleVote>("Multiple");
+
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => x.Id).IsUnique();
+
+        builder.HasIndex(x => x.PollId);
+
+        builder.HasIndex(x => x.UserId);
+    }
+}
