ad49eb995176838a41a83f26a66974ab6070f053
IbadDesktop <kronosxplay243@gmail.com>
2025-05-23 20:43:43 +0400

    feat: add unit of work

---
+++ Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces.Repositories;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly VoteChainDbContext _context;
+
+        public UnitOfWork(VoteChainDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
