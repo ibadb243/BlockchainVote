84f1eea3c38ee9317078abc38a64ccc27d9e60cf
IbadDesktop <kronosxplay243@gmail.com>
2025-05-29 18:16:55 +0400

    refactor: new get block funcs

---
+++ Application/CQRS/GetBlock.cs
@@ -0,0 +1,78 @@
+﻿using Application.Interfaces.Repositories;
+using Ardalis.Result;
+using AutoMapper;
+using Domain.Blockchain;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Caching.Hybrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.GetBlock
+{
+    public class _dto
+    {
+        public DateTime timestamp { get; set; }
+        public string merkle_root { get; set; }
+        public List<BlockTransaction> transactions { get; set; }
+        public string previous_hash { get; set; }
+        public string hash { get; set; }
+        public int nonce { get; set; }
+    }
+
+    public class GetBlockRequest : IRequest<Result<_dto>>
+    {
+        public string? hash { get; set; }
+    }
+
+    public class GetBlockRequestValidator : AbstractValidator<GetBlockRequest>
+    {
+        public GetBlockRequestValidator()
+        {
+            RuleFor(x => x.hash).NotEmpty();
+        }
+    }
+
+    public class GetBlockRequestHandler : IRequestHandler<GetBlockRequest, Result<_dto>>
+    {
+        private readonly IBlockRepository _blockRepository;
+        private readonly HybridCache _cache;
+        private readonly IMapper _mapper;
+
+        public GetBlockRequestHandler(
+            IBlockRepository blockRepository,
+            HybridCache cache,
+            IMapper mapper)
+        {
+            _blockRepository = blockRepository;
+            _cache = cache;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<_dto>> Handle(GetBlockRequest request, CancellationToken cancellationToken)
+        {
+            var cachedBlock = await _cache.GetOrCreateAsync($"block-{request.hash}", async token =>
+            {
+                var block = await _blockRepository.GetByHashAsync(request.hash!, token);
+                return block;
+            },
+            tags: ["block"],
+            cancellationToken: cancellationToken);
+
+            if (cachedBlock == null) return Result.NotFound("Block not found");
+
+            return Result.Success(new _dto
+            {
+                timestamp = cachedBlock.Timestamp,
+                transactions = cachedBlock.Transactions,
+                merkle_root = cachedBlock.MerkleRoot,
+                previous_hash = cachedBlock.PreviousHash,
+                hash = cachedBlock.Hash,
+                nonce = cachedBlock.Nonce,
+            });
+        }
+    }
+}
---
+++ Application/CQRS/GetBlockList.cs
@@ -0,0 +1,71 @@
+﻿using Application.Interfaces.Repositories;
+using Ardalis.Result;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.GetBlockList
+{
+    public class _dto
+    {
+        public DateTime timestamp { get; set; }
+        public string merkle_root { get; set; }
+        public string previous_hash { get; set; }
+        public string hash { get; set; }
+        public int transaction_count { get; set; }
+    }
+
+    public class GetBlockListRequest : IRequest<Result<List<_dto>>>
+    {
+        public int offset { get; set; } = 0;
+        public int limit { get; set; } = 10;
+    }
+
+    public class GetBlockListRequestValidator : AbstractValidator<GetBlockListRequest>
+    {
+        public GetBlockListRequestValidator()
+        {
+            RuleFor(x => x.offset).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.limit).GreaterThanOrEqualTo(1).LessThanOrEqualTo(200);
+        }
+    }
+
+    public class GetBlockListRequestHandler : IRequestHandler<GetBlockListRequest, Result<List<_dto>>>
+    {
+        private readonly IBlockRepository _blockRepository;
+        private readonly IMapper _mapper;
+
+        public GetBlockListRequestHandler(
+            IBlockRepository blockRepository,
+            IMapper mapper)
+        {
+            _blockRepository = blockRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<List<_dto>>> Handle(GetBlockListRequest request, CancellationToken cancellationToken)
+        {
+            var blocks = await _blockRepository
+                .GetAllQuery()
+                .Skip(request.offset)
+                .Take(request.limit)
+                .Select(block => new _dto
+                {
+                    timestamp = block.Timestamp,
+                    merkle_root = block.MerkleRoot,
+                    previous_hash = block.PreviousHash,
+                    hash = block.Hash,
+                    transaction_count = block.Transactions.Count,
+                })
+                .ToListAsync(cancellationToken);
+
+            return Result.Success(blocks);
+        }
+    }
+}
